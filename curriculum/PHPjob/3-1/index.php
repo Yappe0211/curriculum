e441c6c1f7485f68a50ed46179079f7ce5dec5b5
Yappe0211 <99487641+Yappe0211@users.noreply.github.com>
2022-03-18 23:06:39 +0900

    課題提出
    FizzBuzz問題作成

---
+++ curriculum/PHPjob/3-1/index.php
@@ -0,0 +1,18 @@
+<?php
+$num = 0;
+while ($num <= 100) {
+    //echo $num;
+    $num++;
+    
+    if ($num % 3 == 0 && $num % 5 == 0) {
+        echo "FizzBuzz!";
+    } elseif ($num % 5 == 0) {
+        echo "Buzz!";
+    } elseif ($num % 3 == 0) {
+        echo "Fizz!";
+    } else {
+        echo $num;
+    } 
+    echo '<br>';
+}
+?>
