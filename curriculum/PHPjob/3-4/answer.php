7edab5042ee1541f49e5ffcbc9d219947df6675e
Yappe0211 <99487641+Yappe0211@users.noreply.github.com>
2022-06-07 07:57:20 +0900

    3章チェックテスト
    3-4作成しました。
    確認お願いします。

--- curriculum/PHPjob/3-4/answer.php
+++ curriculum/PHPjob/3-4/answer.php
@@ -1,15 +1,60 @@
 <?php 
 //[question.php]から送られてきた名前の変数、選択した回答、問題の答えの変数を作成
+$my_name = $_POST['my_name'];
 $number = $_POST['number'];
+$language = $_POST['language'];
+$command = $_POST['command'];
 //選択した回答と正解が一致していれば「正解！」、一致していなければ「残念・・・」と出力される処理を組んだ関数を作成する
-
+$number = $_POST['number'];
+if ($number == 80) {
+    '正解！';
+}else {
+    '残念・・・';
+}
+$language = $_POST['language'];
+if ($language == "PHP") {
+    '正解！';
+}else {
+    '残念・・・';
+}
+$command = $_POST['command'];
+if ($command == "select") {
+    '正解！';
+}else {
+    '残念・・・';
+}
 ?>
-<p><!--POST通信で送られてきた名前を表示-->さんの結果は・・・？</p>
+
+<link rel="stylesheet" href="css/style.css">
+
+<p><?php echo $my_name; ?><!--POST通信で送られてきた名前を表示-->さんの結果は・・・？</p>
 <p>①の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
-
+<p><?php 
+$number = $_POST['number'];
+if ($number == 80) {
+    echo '正解！';
+}else {
+    echo '残念・・・';
+}
+?></p>
 <p>②の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
-
+<p><?php 
+$language = $_POST['language'];
+if ($language == "PHP") {
+    echo '正解！';
+}else {
+    echo '残念・・・';
+}
+?></p>
 <p>③の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
+<p><?php
+$command = $_POST['command'];
+if ($command == "select") {
+    echo '正解！';
+}else {
+    echo '残念・・・';
+}
+?></p>
--- curriculum/PHPjob/3-4/question.php
+++ curriculum/PHPjob/3-4/question.php
@@ -6,40 +6,41 @@ $number = array(80,22,20,21);
 $language = array("PHP","Python","JAVA","HTML");
 $command = array("join","select","insert","update");
 //② ①で作成した、配列から正解の選択肢の変数を作成してください
-$answer = $number[2];
+$answer = $number[0];
 $answer = $language[0];
-$answer = $command[0];
+$answer = $command[1];
 ?>
 <link rel="stylesheet" href="css/style.css">
 
 <p>お疲れ様です<?php echo $my_name; ?>さん</p>
 <!--フォームの作成 通信はPOST通信で-->
-<form action="answer.php" method="POST">
+<form action="answer.php" method="POST" >
+    <input type="hidden" name="my_name" value="<?php echo $my_name; ?>" />
     <h2>①ネットワークのポート番号は何番？</h2>
-    <?php 
+    <p> <?php 
     $number = [80,22,20,21];
     foreach ($number as $value) { ?> 
         <input type="radio" name="number" value=<?php echo $value; ?> />
         <?php echo $value; ?>
-    <?php } ?>
+    <?php } ?> </p>
     
     <h2>②Webページを作成するための言語は？</h2>
-    <?php
+    <p> <?php
     $language = ["PHP","Python","JAVA","HTML"];
     foreach ($language as $value) { ?>
         <input type="radio" name="language" value="<?php echo $value; ?>"/>
         <?php echo $value; ?>
-    <?php } ?>
+    <?php } ?> </p>
 
     <h2>③MySQLで情報を取得するためのコマンドは？</h2>
-    <?php
+    <p> <?php
     $command = ["join","select","insert","update"];
     foreach ($command as $value) { ?>
         <input type="radio" name="command" value="<?php echo $value; ?>"/>
         <?php echo $value; ?>
-    <?php } ?>
+    <?php } ?> </p>
     <br>
-    <input type="submit" value="回答する" />
+    <p> <input type="submit" value="回答する" /> </p>
 </form>
 
 <!-- 問題の正解の変数と名前の変数を[answer.php]に送る -->
